ce6ec8186b354be9251476730a9c5cb7e5f0e982
G-June <misc.eihwaz@gmail.com>
2013-04-12 20:19:43 +0700

    Update order-opc.js

--- js/order-opc.js
+++ js/order-opc.js
@@ -926,7 +926,7 @@ if (typeof(window.bindInputs) != 'undefined')
 				}
 			});
 			
-			$('input[name=payment_module]').live('click', function() {
+			$('input[name=payment_module]').die('click').live('click', function() {
 				updatePaymentModuleFee();
 			});
 		}
