d4691925de6544f0b61643f86d7cb6b674fffa66
G-June <misc.eihwaz@gmail.com>
2013-04-23 14:11:39 +0700

    Update HTMLTemplateOrderSlip.php

--- gn_override/classes/pdf/HTMLTemplateOrderSlip.php
+++ gn_override/classes/pdf/HTMLTemplateOrderSlip.php
@@ -3,14 +3,12 @@ class HTMLTemplateOrderSlip extends HTMLTemplateOrderSlipCore
 {
 	public function getTaxTabContent() {
 		$invoice_address = new Address((int)$this->order->id_address_invoice);
-		$tax_exempt = Configuration::get('VATNUMBER_MANAGEMENT')
-			&& !empty($invoice_address->vat_number)
-			&& $invoice_address->id_country != Configuration::get('VATNUMBER_COUNTRY');
+		$tax_exempt = Configuration::get('VATNUMBER_MANAGEMENT') && !empty($invoice_address->vat_number) && $invoice_address->id_country != Configuration::get('VATNUMBER_COUNTRY');
 
 		$this->smarty->assign(array(
-			'tax_exempt' => $tax_exempt,
-            'tax_details' => $this->order->getOrderTaxDetails(false, $this->order_slip),
-			'order' => $this->order,
+			'tax_exempt'    => $tax_exempt,
+			'tax_details'   => $this->order->getOrderTaxDetails(false, $this->order_slip),
+			'order'         => $this->order,
 			'is_order_slip' => true
 		));
 
