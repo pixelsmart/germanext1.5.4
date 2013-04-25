0334fce887079a0a0c6a422aaeaa627016a63a9a
G-June <misc.eihwaz@gmail.com>
2013-04-25 14:18:52 +0700

    Update configs.inc.php

--- install/configs.inc.php
+++ install/configs.inc.php
@@ -1,62 +1,62 @@
 <?php
 $_gn_configs = array(
-    'PS_ATTRIBUTE_CATEGORY_DISPLAY' => array(
-        'install'   => 0,
-        'uninstall' => 1
-    ),
-    'PS_ORDER_RETURN_NB_DAYS' => array(
-        'install'   => 7,
-        'uninstall' => 7
-    ),
-    'PS_GN_DB_INSTALLED' => array(
-        'install'   => 1,
-        'uninstall' => 0
-    ),
-    'PS_ORDER_PROCESS_TYPE' => array(
-        'install'   => PS_ORDER_PROCESS_OPC,
-        'uninstall' => PS_ORDER_PROCESS_STANDARD
-    ),
-    'GN_MAIL_CMS' => array(
-        'install'   => 0,
-        'uninstall' => 0,
-        'drop'      => true
-    ),
-    'PS_PRIVACY' => array(
-        'install'   => 0,
-        'uninstall' => 0,
-        'drop'      => true
-    ),
-    'PS_PSTATISTIC' => array(
-        'install'   => 0,
-        'uninstall' => 0,
-        'drop'      => true
-    ),
-    'GN_FORCE_STAT_GATHER' => array(
-        'install'   => 0,
-        'uninstall' => 0,
-        'drop'      => true
-    ),
-    'GN_ALLOW_REORDER' => array(
-        'install'   => 0,
-        'uninstall' => 0,
-        'drop'      => true
-    ),
-    'GN_AVAILABLE_NOW' => array(
-        'install'   => '',
-        'uninstall' => '',
-        'drop'      => true,
-        'lang'      => true
-    ),
-    'GN_AVAILABLE_LATER' => array(
-        'install'   => '',
-        'uninstall' => '',
-        'drop'      => true,
-        'lang'      => true
-    ),
-    'GN_REGISTRATION_TEXT' => array(
-        'install'   => '',
-        'uninstall' => '',
-        'drop'      => true,
-        'lang'      => true
-    )
+	'PS_ATTRIBUTE_CATEGORY_DISPLAY' => array(
+		'install'   => 0,
+		'uninstall' => 1
+	),
+	'PS_ORDER_RETURN_NB_DAYS' => array(
+		'install'   => 7,
+		'uninstall' => 7
+	),
+	'PS_GN_DB_INSTALLED' => array(
+		'install'   => 1,
+		'uninstall' => 0
+	),
+	'PS_ORDER_PROCESS_TYPE' => array(
+		'install'   => PS_ORDER_PROCESS_OPC,
+		'uninstall' => PS_ORDER_PROCESS_STANDARD
+	),
+	'GN_MAIL_CMS' => array(
+		'install'   => 0,
+		'uninstall' => 0,
+		'drop'      => true
+	),
+	'PS_PRIVACY' => array(
+		'install'   => 0,
+		'uninstall' => 0,
+		'drop'      => true
+	),
+	'PS_PSTATISTIC' => array(
+		'install'   => 0,
+		'uninstall' => 0,
+		'drop'      => true
+	),
+	'GN_FORCE_STAT_GATHER' => array(
+		'install'   => 0,
+		'uninstall' => 0,
+		'drop'      => true
+	),
+	'GN_ALLOW_REORDER' => array(
+		'install'   => 0,
+		'uninstall' => 0,
+		'drop'      => true
+	),
+	'GN_AVAILABLE_NOW' => array(
+		'install'   => '',
+		'uninstall' => '',
+		'drop'      => true,
+		'lang'      => true
+	),
+	'GN_AVAILABLE_LATER' => array(
+		'install'   => '',
+		'uninstall' => '',
+		'drop'      => true,
+		'lang'      => true
+	),
+	'GN_REGISTRATION_TEXT' => array(
+		'install'   => '',
+		'uninstall' => '',
+		'drop'      => true,
+		'lang'      => true
+	)
 );
