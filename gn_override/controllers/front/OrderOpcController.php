1c796f168694e30effc22682e3043b004ea18baa
G-June <misc.eihwaz@gmail.com>
2013-05-03 15:43:44 +0700

    Update OrderOpcController.php

--- gn_override/controllers/front/OrderOpcController.php
+++ gn_override/controllers/front/OrderOpcController.php
@@ -14,7 +14,7 @@ class OrderOpcController extends OrderOpcControllerCore
 
 		if ($this->nbProducts) {
 			$this->context->smarty->assign('virtual_cart', false);
-        }
+		}
 		
 		$this->context->smarty->assign('is_multi_address_delivery', $this->context->cart->isMultiAddressDelivery() || ((int)Tools::getValue('multi-shipping') == 1));
 		$this->context->smarty->assign('open_multishipping_fancybox', (int)Tools::getValue('multi-shipping') == 1);
@@ -22,8 +22,8 @@ class OrderOpcController extends OrderOpcControllerCore
 		$this->isLogged = (bool)($this->context->customer->id && Customer::customerIdExistsStatic((int)$this->context->cookie->id_customer));
 
 		if ($this->context->cart->nbProducts()) {
-            if (Tools::isSubmit('callOrder') && Tools::isSubmit('payment_module')) {
-                require_once(_PS_MODULE_DIR_ . 'germanext/payment/manager.php');
+			if (Tools::isSubmit('callOrder') && Tools::isSubmit('payment_module')) {
+				require_once(_PS_MODULE_DIR_ . 'germanext/payment/manager.php');
                 
 				$id_module = Tools::getValue('payment_module');
 				
@@ -47,10 +47,10 @@ class OrderOpcController extends OrderOpcControllerCore
 								$order_href = $this->getOrderHref($id_module);
                         
 								$return = array(
-								   'summary'           => $this->context->cart->getSummaryDetails(), 
-								   'HOOK_TOP_PAYMENT'  => Hook::exec('paymentTop'),
-								   'HOOK_PAYMENT'      => self::presentPaymentHook(),
-								   'BUTTON_ORDER_HREF' => $order_href
+									'summary'           => $this->context->cart->getSummaryDetails(), 
+									'HOOK_TOP_PAYMENT'  => Hook::exec('paymentTop'),
+									'HOOK_PAYMENT'      => self::presentPaymentHook(),
+									'BUTTON_ORDER_HREF' => $order_href
 								);
 
 								if (sizeof($paymentList)) {
@@ -69,10 +69,10 @@ class OrderOpcController extends OrderOpcControllerCore
 											
 											break;
 										}
-                                    }
-                                }
+									}
+								}
 								
-								die( Tools::jsonEncode($return) );
+								die(Tools::jsonEncode($return));
 							}
 							break;
                         
@@ -80,8 +80,11 @@ class OrderOpcController extends OrderOpcControllerCore
 							if (Tools::isSubmit('message')) {
 								$txtMessage = urldecode(Tools::getValue('message'));
 								$this->_updateMessage($txtMessage);
-								if (count($this->errors))
+								
+								if (count($this->errors)) {
 									die('{"hasError" : true, "errors" : ["'.implode('\',\'', $this->errors).'"]}');
+								}
+								
 								die(true);
 							}
 							break;
@@ -89,15 +92,17 @@ class OrderOpcController extends OrderOpcControllerCore
 						case 'updateCarrierAndGetPayments':
 							if ((Tools::isSubmit('delivery_option') || Tools::isSubmit('id_carrier')) && Tools::isSubmit('recyclable') && Tools::isSubmit('gift') && Tools::isSubmit('gift_message')) {
 								$this->_assignWrappingAndTOS();
+								
 								if ($this->_processCarrier()) {
 									$carriers = $this->context->cart->simulateCarriersOutput();
 									$return = array(
-										'summary' => $this->context->cart->getSummaryDetails(),
-										'HOOK_TOP_PAYMENT' => Hook::exec('displayPaymentTop'),
-										'HOOK_PAYMENT' => $this->presentPaymentHook(),
-										'carrier_data' => $this->_getCarrierList(),
+										'summary'            => $this->context->cart->getSummaryDetails(),
+										'HOOK_TOP_PAYMENT'   => Hook::exec('displayPaymentTop'),
+										'HOOK_PAYMENT'       => $this->presentPaymentHook(),
+										'carrier_data'       => $this->_getCarrierList(),
 										'HOOK_BEFORECARRIER' => Hook::exec('displayBeforeCarrier', array('carriers' => $carriers))
 									);
+									
 									Cart::addExtraCarriers($return);
 									die(Tools::jsonEncode($return));
 								}
@@ -128,7 +133,7 @@ class OrderOpcController extends OrderOpcControllerCore
 							break;
 
 						case 'editCustomer':
-							if (!$this->isLogged) {
+							if ( ! $this->isLogged) {
 								exit;
 							}
 
@@ -148,7 +153,7 @@ class OrderOpcController extends OrderOpcControllerCore
 								'token' => Tools::getToken(false)
 							);
 
-							if (!count($this->errors)) {
+							if ( ! count($this->errors)) {
 								$return['isSaved'] = (bool)$this->context->customer->update();
 							}
 							else {
@@ -161,12 +166,12 @@ class OrderOpcController extends OrderOpcControllerCore
 						case 'getAddressBlockAndCarriersAndPayments':
 							if ($this->context->customer->isLogged()) {
 								// check if customer have addresses
-								if (!Customer::getAddressesTotalById($this->context->customer->id)) {
+								if ( ! Customer::getAddressesTotalById($this->context->customer->id)) {
 									die(Tools::jsonEncode(array('no_address' => 1)));
 								}
 
-								if (file_exists(_PS_MODULE_DIR_.'blockuserinfo/blockuserinfo.php')) {
-									include_once(_PS_MODULE_DIR_.'blockuserinfo/blockuserinfo.php');
+								if (file_exists(_PS_MODULE_DIR_ . 'blockuserinfo/blockuserinfo.php')) {
+									include_once(_PS_MODULE_DIR_ . 'blockuserinfo/blockuserinfo.php');
 									$blockUserInfo = new BlockUserInfo();
 								}
 
@@ -177,14 +182,14 @@ class OrderOpcController extends OrderOpcControllerCore
 								$wrapping_fees = $this->context->cart->getGiftWrappingPrice(false);
 								$wrapping_fees_tax_inc = $wrapping_fees = $this->context->cart->getGiftWrappingPrice();
 								$return = array(
-									'summary' => $this->context->cart->getSummaryDetails(),
+									'summary'          => $this->context->cart->getSummaryDetails(),
 									'order_opc_adress' => $this->context->smarty->fetch(_PS_THEME_DIR_.'order-address.tpl'),
-									'block_user_info' => (isset($blockUserInfo) ? $blockUserInfo->hookTop(array()) : ''),
-									'carrier_data' => $this->_getCarrierList(),
+									'block_user_info'  => (isset($blockUserInfo) ? $blockUserInfo->hookTop(array()) : ''),
+									'carrier_data'     => $this->_getCarrierList(),
 									'HOOK_TOP_PAYMENT' => Hook::exec('displayPaymentTop'),
-									'HOOK_PAYMENT' => $this->presentPaymentHook(),
-									'no_address' => 0,
-									'gift_price' => Tools::displayPrice(Tools::convertPrice(Product::getTaxCalculationMethod() == 1 ? $wrapping_fees : $wrapping_fees_tax_inc, new Currency((int)($this->context->cookie->id_currency))))
+									'HOOK_PAYMENT'     => $this->presentPaymentHook(),
+									'no_address'       => 0,
+									'gift_price'       => Tools::displayPrice(Tools::convertPrice(Product::getTaxCalculationMethod() == 1 ? $wrapping_fees : $wrapping_fees_tax_inc, new Currency((int)($this->context->cookie->id_currency))))
 								);
 								die(Tools::jsonEncode($return));
 							}
@@ -195,8 +200,11 @@ class OrderOpcController extends OrderOpcControllerCore
 							/* Bypass payment step if total is 0 */
 							if (($id_order = $this->_checkFreeOrder()) && $id_order) {
 								$email = $this->context->customer->email;
-								if ($this->context->customer->is_guest)
+								
+								if ($this->context->customer->is_guest) {
 									$this->context->customer->logout(); // If guest we clear the cookie for security reason
+								}
+								
 								die('freeorder:'.$id_order.':'.$email);
 							}
 							exit;
@@ -206,6 +214,7 @@ class OrderOpcController extends OrderOpcControllerCore
 							if ($this->context->customer->isLogged(true)) {
 								$address_delivery = new Address((int)(Tools::getValue('id_address_delivery')));
 								$address_invoice = ((int)(Tools::getValue('id_address_delivery')) == (int)(Tools::getValue('id_address_invoice')) ? $address_delivery : new Address((int)(Tools::getValue('id_address_invoice'))));
+								
 								if ($address_delivery->id_customer != $this->context->customer->id || $address_invoice->id_customer != $this->context->customer->id) {
 									$this->errors[] = Tools::displayError('This address is not yours.');
 								}
@@ -233,11 +242,11 @@ class OrderOpcController extends OrderOpcControllerCore
 										$wrapping_fees = $this->context->cart->getGiftWrappingPrice(false);
 										$wrapping_fees_tax_inc = $wrapping_fees = $this->context->cart->getGiftWrappingPrice();
 										$result = array_merge($result, array(
-											'summary' => $this->context->cart->getSummaryDetails(),
+											'summary'          => $this->context->cart->getSummaryDetails(),
 											'HOOK_TOP_PAYMENT' => Hook::exec('displayPaymentTop'),
-											'HOOK_PAYMENT' => $this->presentPaymentHook(),
-											'gift_price' => Tools::displayPrice(Tools::convertPrice(Product::getTaxCalculationMethod() == 1 ? $wrapping_fees : $wrapping_fees_tax_inc, new Currency((int)($this->context->cookie->id_currency)))),
-											'carrier_data' => $this->_getCarrierList()
+											'HOOK_PAYMENT'     => $this->presentPaymentHook(),
+											'gift_price'       => Tools::displayPrice(Tools::convertPrice(Product::getTaxCalculationMethod() == 1 ? $wrapping_fees : $wrapping_fees_tax_inc, new Currency((int)($this->context->cookie->id_currency)))),
+											'carrier_data'     => $this->_getCarrierList()
 										));
 										die(Tools::jsonEncode($result));
 									}
@@ -266,6 +275,7 @@ class OrderOpcController extends OrderOpcControllerCore
 
 						case 'cartReload':
 							$this->_assignSummaryInformations();
+							
 							if ($this->context->customer->id) {
 								$this->context->smarty->assign('address_list', $this->context->customer->getAddresses($this->context->language->id));
 							}
@@ -298,117 +308,121 @@ class OrderOpcController extends OrderOpcControllerCore
 		}
 	}
     
-    public function getOrderHref($id_module = false) {
-        $id_module = $id_module ? $id_module : $this->context->cart->id_payment;
+	public function getOrderHref($id_module = false) {
+		$id_module = $id_module ? $id_module : $this->context->cart->id_payment;
+
+		if ((int)$id_module != 0) {
+			return $this->context->link->getPageLink('order-opc.php', true, null, array('callOrder' => true, 'payment_module' => $id_module));
+		}
         
-        if ((int)$id_module != 0) {
-            return $this->context->link->getPageLink('order-opc.php', true, null, array('callOrder' => true, 'payment_module' => $id_module));
-        }
-        
-        return ;
-    }
+		return ;
+	}
     
-    protected function _checkPaymentError($check_conditions) {
+	protected function _checkPaymentError($check_conditions) {
 		if ( ! $this->isLogged) {
 			return '<p class="warning">'.Tools::displayError('Please sign in to see payment methods.').'</p>';
-        }
+		}
         
 		if ($this->context->cart->OrderExists()) {
 			return '<p class="warning">'.Tools::displayError('Error: This order has already been validated.').'</p>';
-        }
+		}
         
-		if (!$this->context->cart->id_customer
-            || !Customer::customerIdExistsStatic($this->context->cart->id_customer)
-            || Customer::isBanned($this->context->cart->id_customer)) {
+		if ( ! $this->context->cart->id_customer
+			|| !Customer::customerIdExistsStatic($this->context->cart->id_customer)
+			|| Customer::isBanned($this->context->cart->id_customer)) 
+		{
 			return '<p class="warning">'.Tools::displayError('Error: No customer.').'</p>';
-        }
+		}
         
 		$address_delivery = new Address($this->context->cart->id_address_delivery);
 		$address_invoice = ($this->context->cart->id_address_delivery == $this->context->cart->id_address_invoice ? $address_delivery : new Address($this->context->cart->id_address_invoice));
-		if (!$this->context->cart->id_address_delivery
-            || !$this->context->cart->id_address_invoice
-            || !Validate::isLoadedObject($address_delivery)
-            || !Validate::isLoadedObject($address_invoice)
-            || $address_invoice->deleted
-            || $address_delivery->deleted) {
+		if ( ! $this->context->cart->id_address_delivery
+			|| ! $this->context->cart->id_address_invoice
+			|| ! Validate::isLoadedObject($address_delivery)
+			|| ! Validate::isLoadedObject($address_invoice)
+			|| $address_invoice->deleted
+			|| $address_delivery->deleted) 
+		{
 			return '<p class="warning">'.Tools::displayError('Error: Please choose an address.').'</p>';
-        }
+		}
         
 		if (count($this->context->cart->getDeliveryOptionList()) == 0) {
 			if ($this->context->cart->isMultiAddressDelivery()) {
 				return '<p class="warning">'.Tools::displayError('Error: None of your chosen carriers deliver to some of  the addresses you\'ve selected.').'</p>';
-            } 
+			} 
 			else {
 				return '<p class="warning">'.Tools::displayError('Error: None of your chosen carriers deliver to the address you\'ve selected.').'</p>';
-            }
+			}
 		}
         
-		if (!$this->context->cart->getDeliveryOption(null, false) && !$this->context->cart->isVirtualCart()) {
+		if ( ! $this->context->cart->getDeliveryOption(null, false) && !$this->context->cart->isVirtualCart()) {
 			return '<p class="warning">'.Tools::displayError('Error: Please choose a carrier.').'</p>';
-        }
+		}
         
-		if (!$this->context->cart->id_currency) {
+		if ( ! $this->context->cart->id_currency) {
 			return '<p class="warning">'.Tools::displayError('Error: No currency has been selected.').'</p>';
-        }
+		}
         
 		if ($check_conditions && ! $this->context->cookie->checkedTOS && Configuration::get('PS_CONDITIONS')) {
 			return '<p class="warning">'.Tools::displayError('Please accept the Terms of Service.').'</p>';
-        }
+		}
 
 		/* If some products have disappear */
-		if (!$this->context->cart->checkQuantities()) {
+		if ( ! $this->context->cart->checkQuantities()) {
 			return '<p class="warning">'.Tools::displayError('An item in your cart is no longer available. You cannot proceed with your order.').'</p>';
-        }
+		}
 
 		/* Check minimal amount */
 		$currency = Currency::getCurrency((int)$this->context->cart->id_currency);
 
 		$minimalPurchase = Tools::convertPrice((float)Configuration::get('PS_PURCHASE_MINIMUM'), $currency);
+		
 		if ($this->context->cart->getOrderTotal(false, Cart::ONLY_PRODUCTS) < $minimalPurchase) {
  			return '<p class="warning">'.sprintf(
 				Tools::displayError('A minimum purchase total of %s is required in order to validate your order.'),
  				Tools::displayPrice($minimalPurchase, $currency)
  			).'</p>';
-        }
+		}
 
 		return 0;
-    }
+	}
     
-    protected function _getPaymentMethods() {
-        $error = $this->_checkPaymentError(true);
+	protected function _getPaymentMethods() {
+		$error = $this->_checkPaymentError(true);
         
-        if ($error) {
-            return $error;
-        }
+		if ($error) {
+			return $error;
+		}
         
-        $return = Hook::exec('displayPayment');
+		$return = Hook::exec('displayPayment');
         
-		if (!$return) {
+		if ( ! $return) {
 			return '<p class="warning">'.Tools::displayError('No payment method is available for use at this time. ').'</p>';
-        }
+		}
+		
 		return $return;
-    }
+	}
     
 	protected function _assignPayment() {
 		$this->context->smarty->assign(array(
-			'HOOK_TOP_PAYMENT' => ($this->isLogged ? Hook::exec('displayPaymentTop') : ''),
-			'HOOK_PAYMENT' => $this->presentPaymentHook(),
-            'BUTTON_ORDER_HREF' => $this->getOrderHref()
+			'HOOK_TOP_PAYMENT'  => ($this->isLogged ? Hook::exec('displayPaymentTop') : ''),
+			'HOOK_PAYMENT'      => $this->presentPaymentHook(),
+			'BUTTON_ORDER_HREF' => $this->getOrderHref()
 		));
 	}
     
 	private function presentPaymentHook() {
 		if ((int)Configuration::get('GN_CHECK_PAYMENT') != 1 || self::$is_mobile) {
 			return parent::_getPaymentMethods();
-        }
+		}
         
-        require_once(_PS_MODULE_DIR_ . 'germanext/payment/manager.php');
+		require_once(_PS_MODULE_DIR_ . 'germanext/payment/manager.php');
 
-        $error = $this->_checkPaymentError(false);
+		$error = $this->_checkPaymentError(false);
 		
 		if ($error) {
 			return $error;
-        }
+		}
 
 		$paymentCost = new PaymentCost();
 		$paymentList = $paymentCost->getPaymentList();
@@ -425,11 +439,11 @@ class OrderOpcController extends OrderOpcControllerCore
 			if ($result !== false) {
 				if ( ! is_array($result)) {
 					$result = array($result);
-                }
+				}
 					
 				foreach ($result as $method) {
 					$modules[] = array('id' => $id_payment, 'content' => $method);
-                }
+				}
 			}
 		}
 		
@@ -443,7 +457,7 @@ class OrderOpcController extends OrderOpcControllerCore
 		}
 		
 		return $HOOK_PAYMENT;
-    }
+	}
 	
 	protected function _getCarrierList() {
 		$result = parent::_getCarrierList();
